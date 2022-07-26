ced0cb0ac679f8e5ac03049e6fac9b32bb77ab60
handagyeong <80438961+handagyeong@users.noreply.github.com>
2022-07-27 03:25:35 +0900

    upanddown

---
+++ maintest/Assets/UpAndDown/UpAndDownBtnType.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class UpAndDownBtnType : MonoBehaviour
+{
+
+    public UpAndDownBTNType currentType;
+    public void OnBtnClick()
+    {
+        switch (currentType)
+        {
+            case UpAndDownBTNType.Start:
+                Debug.Log("start!");
+                break;
+            case UpAndDownBTNType.num1:
+                Debug.Log("num1");
+                break;
+            case UpAndDownBTNType.num2:
+                Debug.Log("num2");
+                break;
+            case UpAndDownBTNType.num3:
+                Debug.Log("num3");
+                break;
+            case UpAndDownBTNType.num4:
+                Debug.Log("num4");
+                break;
+            case UpAndDownBTNType.num5:
+                Debug.Log("num5");
+                break;
+            case UpAndDownBTNType.num6:
+                Debug.Log("num6");
+                break;
+            case UpAndDownBTNType.num7:
+                Debug.Log("num7");
+                break;
+            case UpAndDownBTNType.num8:
+                Debug.Log("num8");
+                break;
+            case UpAndDownBTNType.num9:
+                Debug.Log("num9");
+                break;
+            case UpAndDownBTNType.num0:
+                Debug.Log("num0");
+                break;
+
+
+        }
+    }
+
+}
---
+++ maintest/Assets/UpAndDown/UpAndDownUI.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UpAndDownBTNType
+{
+    Start,num1,num2,num3,num4,num5,num6,num7,num8, num9,num0
+
+}
+public class UpAndDownUI : MonoBehaviour
+{
+
+}
