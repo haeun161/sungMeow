09d8fc9369fe0760ac48eabd94d9af0c7aca3de1
이하은 <haeun161@naver.com>
2022-07-27 19:53:39 +0900

    페이지 이동 과정중 오류 고침

--- maintest/Assets/FindtheCat/FindtheCatBtnType.cs
+++ maintest/Assets/FindtheCat/FindtheCatBtnType.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FindtheCatBtnType : MonoBehaviour
 {
@@ -9,25 +10,28 @@ public class FindtheCatBtnType : MonoBehaviour
     {
         Debug.Log("1차 테스트");
         int result = Random.Range(0, 3);
-        int trial = 0;
 
+        //for(int trial=0; trial<3;trial++)
         switch (currentType)
         {
-            case FindtheCatBTNType.start:
+            case FindtheCatBTNType.start1:
                 Debug.Log("게임 시작");
+                SceneManager.LoadScene("FindtheCat2");
                 break;
-                
-                
+
+            case FindtheCatBTNType.start2:                
+                Debug.Log("게임을 시작합니다!");
+                SceneManager.LoadScene("FindtheCat3");
+                break;
+
             case FindtheCatBTNType.TreeButton:
                 if (result == 0)
                 {
                     Debug.Log("고양이를 찾았어요!!");
-                    trial++;
                 }
                 else
                 {
                     Debug.Log("아쉽지만 고양이가 여기에는 없어요:(");
-                    trial++;
                 }
                 break;
 
@@ -35,24 +39,20 @@ public class FindtheCatBtnType : MonoBehaviour
                 if (result == 1)
                 {
                     Debug.Log("고양이를 찾았어요!!");
-                    trial++;
                 }
                 else
                 {
                     Debug.Log("아쉽지만 고양이가 여기에는 없어요:(");
-                    trial++;
                 }
                 break;
             case FindtheCatBTNType.WallButton:
                 if (result == 2)
                 {
                     Debug.Log("고양이를 찾았어요!!");
-                    trial++;
                 }
                 else
                 {
                     Debug.Log("아쉽지만 고양이가 여기에는 없어요:(");
-                    trial++;
                 }
                 break;
 
--- maintest/Assets/FindtheCat/FindtheCatUI.cs
+++ maintest/Assets/FindtheCat/FindtheCatUI.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public enum FindtheCatBTNType
 {
-    start,
+    start1,
+    start2,
     TreeButton,
     RoofButton,
     WallButton
