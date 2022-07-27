3a4ca768876ca88786b203cf0f2d85893431b3b0
elive7 <parksub020704@gmail.com>
2022-07-27 11:35:24 +0900

    Revert "aa"
    This reverts commit c592b461d778c5061345de0eaccbe54f253b53c0, reversing
    changes made to be84e020941c1ef907010ce252bb79b773ab6b87.

--- maintest/Assets/avoidRain/avoidRainBtnType.cs
+++ maintest/Assets/avoidRain/avoidRainBtnType.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 
 public class avoidRainBtnType : MonoBehaviour
@@ -14,10 +13,8 @@ public class avoidRainBtnType : MonoBehaviour
         {
             case avoidRainBTNType.start1:
                 Debug.Log("시작1");
-                SceneManager.LoadScene("avoidRain2");
                 break;
             case avoidRainBTNType.start2:
-                SceneManager.LoadScene("avoidRain3");
                 Debug.Log("시작2");
                 break;
             case avoidRainBTNType.left:
