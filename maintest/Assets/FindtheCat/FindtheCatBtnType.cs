d9fc116b2451b5685b0551a37f377093a0d69d66
이하은 <haeun161@naver.com>
2022-07-27 19:25:13 +0900

    FindtheCat 60%완료

--- maintest/Assets/FindtheCat/FindtheCatBtnType.cs
+++ maintest/Assets/FindtheCat/FindtheCatBtnType.cs
@@ -7,19 +7,53 @@ public class FindtheCatBtnType : MonoBehaviour
     public FindtheCatBTNType currentType;
     public void OnBtnClick()
     {
+        Debug.Log("1차 테스트");
+        int result = Random.Range(0, 3);
+        int trial = 0;
+
         switch (currentType)
         {
             case FindtheCatBTNType.start:
                 Debug.Log("게임 시작");
                 break;
+                
+                
             case FindtheCatBTNType.TreeButton:
-                Debug.Log("나무 위에 고양이가 있어요!");
+                if (result == 0)
+                {
+                    Debug.Log("고양이를 찾았어요!!");
+                    trial++;
+                }
+                else
+                {
+                    Debug.Log("아쉽지만 고양이가 여기에는 없어요:(");
+                    trial++;
+                }
                 break;
+
             case FindtheCatBTNType.RoofButton:
-                Debug.Log("지붕 위에 고양이가 있어요!");
+                if (result == 1)
+                {
+                    Debug.Log("고양이를 찾았어요!!");
+                    trial++;
+                }
+                else
+                {
+                    Debug.Log("아쉽지만 고양이가 여기에는 없어요:(");
+                    trial++;
+                }
                 break;
             case FindtheCatBTNType.WallButton:
-                Debug.Log("담장 위에 고양이가 있어요!");
+                if (result == 2)
+                {
+                    Debug.Log("고양이를 찾았어요!!");
+                    trial++;
+                }
+                else
+                {
+                    Debug.Log("아쉽지만 고양이가 여기에는 없어요:(");
+                    trial++;
+                }
                 break;
 
         }
