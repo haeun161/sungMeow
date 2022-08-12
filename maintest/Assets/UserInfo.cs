7b667a034216d4adedbe9b8a89691c9af8d68c4b
이하은 <haeun161@naver.com>
2022-08-12 13:06:23 +0900

    데이터베이스,PHP 사용하여 인벤토리 아이템 불러오기 성공

---
+++ maintest/Assets/UserInfo.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInfo : MonoBehaviour
+{
+    public string UserName { get; private set; }
+    string UserPassword;
+    string Email;
+    string Crystal;
+    
+    public void SetUsername(string username)
+    {
+        UserName = username;
+        Debug.Log(UserName);
+    }
+    public void SetPassword(string userpassword)
+    {
+
+        UserPassword = userpassword;
+        Debug.Log(UserPassword);
+    }
+    
+
+    
+}
---
+++ maintest/Assets/Web.cs
@@ -0,0 +1,133 @@
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+// UnityWebRequest.Get example
+
+// Access a website and use UnityWebRequest.Get to download a page.
+// Also try to download a non-existing page. Display the error.
+
+public class Web : MonoBehaviour
+{
+    void Start()
+    {
+        //StartCoroutine(GetDate());
+        //StartCoroutine(Login("haeun","pw1234"));
+        //StartCoroutine(RegisterUser("subin2", "pw12342", "1@gmail.com"));
+    }
+
+    public void ShowUserItems()
+    {
+        StartCoroutine(GetItemsName(MainScript.Instance.UserInfo.UserName));
+    }
+
+    IEnumerator GetDate()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get("http://localhost:8080/UnityBackendTutorial/GetUsers.php"))
+        {
+            // Request and wait for the desired page.
+            yield return www.Send();
+
+            if(www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log(www.downloadHandler.text);
+
+                byte[] results = www.downloadHandler.data;
+            }
+           
+        }
+    }
+    public IEnumerator GetUsers()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get("http://localhost:8080/UnityBackendTutorial/GetUsers.php"))
+        {
+            // Request and wait for the desired page.
+            yield return www.Send();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log(www.downloadHandler.text);
+
+                byte[] results = www.downloadHandler.data;
+            }
+
+        }
+    }
+    public IEnumerator Login(string username, string password)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("loginUser", username);
+        form.AddField("loginPassword", password);
+        using (UnityWebRequest www = UnityWebRequest.Post("http://localhost:8080/UnityBackendTutorial/Login.php", form))
+        {
+            yield return www.SendWebRequest();
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log(www.downloadHandler.text);
+                //MainScript.Instance.UserInfo.SetInfo(username, password);
+                MainScript.Instance.UserInfo.SetUsername(username);
+
+                // MainScript.Instance.UserInfo.SetUsername(www.downloadHandler.text);
+                //MainScript.Instance.UserInfo.SetPassword(password);
+            }
+        }
+    }
+    public IEnumerator RegisterUser(string username, string password, string email)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("loginUser", username);
+        form.AddField("loginPassword", password);
+        form.AddField("loginEmail", email);
+
+        using (UnityWebRequest www = UnityWebRequest.Post("http://localhost:8080/UnityBackendTutorial/RegisterUser.php", form))
+        {
+            yield return www.SendWebRequest();
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log(www.downloadHandler.text);
+            }
+        }
+    }
+    public IEnumerator GetItemsName(string username)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("username", username);
+
+        using (UnityWebRequest www = UnityWebRequest.Post("http://localhost:8080/UnityBackendTutorial/GetItemsName.php", form))
+        {
+            // Request and wait for the desired page.
+            yield return www.Send();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log(www.downloadHandler.text);
+                string jsonArray = www.downloadHandler.text;
+
+                //Call callback function to pass results
+            }
+
+        }
+    }
+
+}
+
